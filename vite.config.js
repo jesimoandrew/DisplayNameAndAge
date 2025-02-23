025a42a876cb20787ef17cdb19c947a512b0497d
Andrew Areglado <areglado.jesimoandrew@gmail.com>
2025-02-23 16:16:47 +0800

    z

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/DisplayNameAndAge/', // Change this to your actual GitHub repo name
+});
